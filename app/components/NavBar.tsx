24adeac45561da9f857aa92bc64b1b8d9f410488
alekspetkoff <iskander9207@icloud.com>
2025-01-03 18:36:28 +0200

    test

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,49 +4,49 @@ import Link from "next/link";
 import React from "react";
 
 export async function NavBar() {
-  const session = await auth();
-  console.log(session);
-  return (
-    <header className="px-5 py-3 bg-white shadow-sm font-work-sans max-w-[1240px] mx-auto">
-      <nav className="flex justify-between items-center">
-        <Link href="/">
-          <Image
-            src="/logo.png"
-            alt="YC Directory logo"
-            width={144}
-            height={30}
-          />
-        </Link>
-        <div className="flex items-center gap-5 text-black font-medium">
-          {session && session?.user ? (
-            <>
-              <Link href="/startup/create">
-                <span>Create</span>
-              </Link>
-              <form
-                action={async () => {
-                  "use server";
-                  await signOut({ redirectTo: "/" });
-                }}
-              >
-                <button type="submit">Logout</button>
-              </form>
-              {/* <Link href={`/user/${session?.id}`}>
+    const session = await auth();
+
+    return (
+        <header className="px-5 py-3 bg-white shadow-sm font-work-sans max-w-[1240px] mx-auto">
+            <nav className="flex justify-between items-center">
+                <Link href="/">
+                    <Image
+                        src="/logo.png"
+                        alt="YC Directory logo"
+                        width={144}
+                        height={30}
+                    />
+                </Link>
+                <div className="flex items-center gap-5 text-black font-medium">
+                    {session && session?.user ? (
+                        <>
+                            <Link href="/startup/create">
+                                <span>Create</span>
+                            </Link>
+                            <form
+                                action={async () => {
+                                    "use server";
+                                    await signOut({ redirectTo: "/" });
+                                }}
+                            >
+                                <button type="submit">Logout</button>
+                            </form>
+                            {/* <Link href={`/user/${session?.id}`}>
                 <span>{session?.user?.name}</span>
               </Link> */}
-            </>
-          ) : (
-            <form
-              action={async () => {
-                "use server";
-                await signIn("github");
-              }}
-            >
-              <button type="submit">Login</button>
-            </form>
-          )}
-        </div>
-      </nav>
-    </header>
-  );
+                        </>
+                    ) : (
+                        <form
+                            action={async () => {
+                                "use server";
+                                await signIn("github");
+                            }}
+                        >
+                            <button type="submit">Login</button>
+                        </form>
+                    )}
+                </div>
+            </nav>
+        </header>
+    );
 }
