2d6ab91b44d21ac2bed556940efc509f5d481476
alekspetkoff <iskander9207@icloud.com>
2025-01-04 15:25:50 +0200

    moved the components outside of app dir

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,12 @@
-import { NavBar } from "../components/NavBar";
+import { NavBar } from "../../components/NavBar";
 
 export default function Layout({
-  children,
+    children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <main className="font-work-sans">
-      <NavBar />
-      {children}
-    </main>
-  );
+    return (
+        <main className="font-work-sans">
+            <NavBar />
+            {children}
+        </main>
+    );
 }
