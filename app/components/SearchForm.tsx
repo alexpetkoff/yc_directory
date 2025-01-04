64c0555dc87879b73a099f3bf907e15897b4ac2c
alekspetkoff <iskander9207@icloud.com>
2025-01-04 15:24:23 +0200

    added react-lucide

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "next/form";
 import SearchFormReset from "./SearchFormReset";
+import { Search } from "lucide-react";
 
 function SearchForm({ query }: { query?: string }) {
     return (
@@ -14,7 +15,7 @@ function SearchForm({ query }: { query?: string }) {
             <div className="flex gap-2">
                 {query && <SearchFormReset />}
                 <button type="submit" className="search-btn text-white">
-                    S
+                    <Search className="size-5" />
                 </button>
             </div>
         </Form>
