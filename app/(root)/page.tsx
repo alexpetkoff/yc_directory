1e526ff73557e48264a763096a34f7aa80697a94
alekspetkoff <iskander9207@icloud.com>
2025-01-05 14:58:59 +0200

    created StartupTypeCard and startupCard and grid

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+import { StartupTypeCard } from "@/types/types";
 
 export default async function Home({
     searchParams,
@@ -7,16 +9,46 @@ export default async function Home({
 }) {
     const query = (await searchParams).query || "";
 
+    const posts: StartupTypeCard[] = [
+        {
+            _createdAt: new Date().toISOString(),
+            views: 55,
+            author: { _id: 1, name: "Alexander" },
+            _id: 1,
+            description: "This is a description",
+            image: "https://future-business.org/wp-content/uploads/2022/01/dreamstime_xxl_107550237-scaled.jpg",
+            category: "Robots",
+            title: "We Robots",
+        },
+    ];
+
     return (
-        <section className="pink_container">
-            <h1 className="heading">
-                Pitch your startup, <br /> Connect with Entreprenuers
-            </h1>
-            <p className="sub-heading !max-w-3xl">
-                Submit ideas, Vote on pitches, and get Noticed in Virtual
-                Competitions.
-            </p>
-            <SearchForm query={query} />
-        </section>
+        <>
+            <section className="pink_container">
+                <h1 className="heading">
+                    Pitch your startup, <br /> Connect with Entreprenuers
+                </h1>
+                <p className="sub-heading !max-w-3xl">
+                    Submit ideas, Vote on pitches, and get Noticed in Virtual
+                    Competitions.
+                </p>
+                <SearchForm query={query} />
+            </section>
+
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for "${query}"` : "All Startups"}
+                </p>
+                <ul className="mt-7 card_grid">
+                    {posts?.length > 0 ? (
+                        posts.map((post, index: number) => (
+                            <StartupCard key={post?._id} post={post} />
+                        ))
+                    ) : (
+                        <p className="no-results">No startups found!</p>
+                    )}
+                </ul>
+            </section>
+        </>
     );
 }
---
+++ types/types.ts
@@ -0,0 +1,13 @@
+export type StartupTypeCard = {
+    _createdAt: string;
+    views: number;
+    author: {
+        _id: string | number;
+        name: string;
+    };
+    title: string;
+    category: string;
+    _id: string | number;
+    image: string;
+    description: string;
+};
