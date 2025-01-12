201d3c7c77cb1665a755e5974ac299813c6dd0e9
alekspetkoff <iskander9207@icloud.com>
2025-01-12 20:58:29 +0200

    ...

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,42 +4,45 @@ import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
-  searchParams,
+    searchParams,
 }: {
-  searchParams: Promise<{ query?: string }>;
+    searchParams: Promise<{ query?: string }>;
 }) {
-  const query = (await searchParams).query || "";
-  const params = { search: query || null };
-  let { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+    const query = (await searchParams).query || "";
+    const params = { search: query || null };
+    let { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
-  return (
-    <>
-      <section className="pink_container">
-        <h1 className="heading">
-          Pitch your startup, <br /> Connect with Entreprenuers
-        </h1>
-        <p className="sub-heading !max-w-3xl">
-          Submit ideas, Vote on pitches, and get Noticed in Virtual
-          Competitions.
-        </p>
-        <SearchForm query={query} />
-      </section>
+    return (
+        <>
+            <section className="pink_container">
+                <h1 className="heading">
+                    Pitch your startup, <br /> Connect with Entreprenuers
+                </h1>
+                <p className="sub-heading !max-w-3xl">
+                    Submit ideas, Vote on pitches, and get Noticed in Virtual
+                    Competitions.
+                </p>
+                <SearchForm query={query} />
+            </section>
 
-      <section className="section_container">
-        <p className="text-30-semibold">
-          {query ? `Search results for "${query}"` : "All Startups"}
-        </p>
-        <ul className="mt-7 card_grid">
-          {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard) => (
-              <StartupCard key={post?._id} post={post as StartupTypeCard} />
-            ))
-          ) : (
-            <p className="no-results">No startups found!</p>
-          )}
-        </ul>
-      </section>
-      <SanityLive />
-    </>
-  );
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for "${query}"` : "All Startups"}
+                </p>
+                <ul className="mt-7 card_grid">
+                    {posts?.length > 0 ? (
+                        posts.map((post: StartupTypeCard) => (
+                            <StartupCard
+                                key={post?._id}
+                                post={post as StartupTypeCard}
+                            />
+                        ))
+                    ) : (
+                        <p className="no-results">No startups found!</p>
+                    )}
+                </ul>
+            </section>
+            <SanityLive />
+        </>
+    );
 }
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -7,61 +7,68 @@ import Link from "next/link";
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
 function StartupCard({ post }: { post: StartupTypeCard }) {
-  const {
-    _createdAt,
-    views,
-    author,
-    title,
-    category,
-    _id,
-    image,
-    description,
-  } = post;
+    const {
+        _createdAt,
+        views,
+        author,
+        title,
+        category,
+        _id,
+        image,
+        description,
+    } = post;
 
-  return (
-    <li className="startup-card group">
-      <div className="flex-between">
-        <p className="startup_card_date">{formatDate(_createdAt)}</p>
-        <div className="flex gap-1.5">
-          <EyeIcon className="size-6 text-primary" />
-          <span className="text-16-medium">{views}</span>
-        </div>
-      </div>
-      <div className="flex-between mt-5 gap-5">
-        <div className="flex-1">
-          <Link href={`/user/${author?._id}`}>
-            <p className="text-16-medium line-clamp-1">
-              <span className="text-primary">Author:</span> {author?.name}
-            </p>
-          </Link>
-          <Link href={`/startup/${_id}`}>
-            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
-          </Link>
-        </div>
-        <Link href={`/user/${author?._id}`}>
-          <Image
-            src="https://placehold.co/48x48"
-            alt="placeholder"
-            width={48}
-            height={48}
-            className="rounded-full"
-          />
-        </Link>
-      </div>
-      <Link href={`/startup/${_id}`}>
-        <p className="startup-card_desc">{description}</p>
-        <img src={image} className="startup-card_img" alt="placeholder" />
-      </Link>
-      <div className="flex-between gap-3 mt-5">
-        <Link href={`/?query=${category?.toLowerCase()}`}>
-          <p className="text-16-medium">{category}</p>
-        </Link>
-        <button className="startup-card_btn">
-          <Link href={`/startup/${_id}`}>Details</Link>
-        </button>
-      </div>
-    </li>
-  );
+    return (
+        <li className="startup-card group">
+            <div className="flex-between">
+                <p className="startup_card_date">{formatDate(_createdAt)}</p>
+                <div className="flex gap-1.5">
+                    <EyeIcon className="size-6 text-primary" />
+                    <span className="text-16-medium">{views}</span>
+                </div>
+            </div>
+            <div className="flex-between mt-5 gap-5">
+                <div className="flex-1">
+                    <Link href={`/user/${author?._id}`}>
+                        <p className="text-16-medium line-clamp-1">
+                            <span className="text-primary">Author:</span>{" "}
+                            {author?.name}
+                        </p>
+                    </Link>
+                    <Link href={`/startup/${_id}`}>
+                        <h3 className="text-26-semibold line-clamp-1">
+                            {title}
+                        </h3>
+                    </Link>
+                </div>
+                <Link href={`/user/${author?._id}`}>
+                    <Image
+                        src="https://placehold.co/48x48"
+                        alt="placeholder"
+                        width={48}
+                        height={48}
+                        className="rounded-full"
+                    />
+                </Link>
+            </div>
+            <Link href={`/startup/${_id}`}>
+                <p className="startup-card_desc">{description}</p>
+                <img
+                    src={image}
+                    className="startup-card_img"
+                    alt="placeholder"
+                />
+            </Link>
+            <div className="flex-between gap-3 mt-5">
+                <Link href={`/?query=${category?.toLowerCase()}`}>
+                    <p className="text-16-medium">{category}</p>
+                </Link>
+                <button className="startup-card_btn">
+                    <Link href={`/startup/${_id}`}>Details</Link>
+                </button>
+            </div>
+        </li>
+    );
 }
 
 export default StartupCard;
