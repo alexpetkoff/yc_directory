8cb24e14db01c3a6edf3fa8b9fb0711c795eb4ab
Alexander Petkov <iskander9207@icloud.com>
2025-01-17 15:34:31 +0200

    created startupdetails page with ppr

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,5 +1,59 @@
+import { client } from "@/sanity/lib/client";
+import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { formatDate } from "@/utils/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 import React from "react";
 
-export function Page() {
-  return <></>;
+export const experimental_ppr = true;
+
+export default async function StartUpDetailsPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  if (!post) return notFound();
+
+  return (
+    <>
+      <section className="pink_container !min-h-[230px]">
+        <p className="tag">{formatDate(post?._createdAt)}</p>
+        <h1 className="heading">{post?.title}</h1>
+        <p className="sub-heading !max-w-5xl">{post?.description}</p>
+      </section>
+      <section className="section_container">
+        <img
+          src={post?.image || ""}
+          alt="thumbnail"
+          className="w-1/2 max-h-[300px] rounded-xl mx-auto object-cover"
+        />
+        <div className="space-y-5 mt-10 max-w-4xl mx-auto">
+          <div className="flex-between gap-5">
+            <Link href={`/user/${post?.author?._id}`}>
+              <Image
+                src={post?.author?.image}
+                alt="avatar"
+                width={100}
+                height={100}
+              />
+              <div className="">
+                <p className="text-20-medium">{post?.author?.name}</p>
+                <p className="text-16-medium !text-black-300">
+                  @{post?.author?.username}
+                </p>
+              </div>
+            </Link>
+            <p className="category-tag">{post?.category}</p>
+          </div>
+          <h3 className="text-30-bold">Pitch Details:</h3>
+          <div className="">{post?.pitch}</div>
+        </div>
+      </section>
+    </>
+  );
 }
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY = defineQuery(
-    `*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+  `*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
     _id, 
      title, 
     slug, 
@@ -14,4 +14,23 @@ export const STARTUPS_QUERY = defineQuery(
     category, 
     image
 }`
+);
+
+export const STARTUP_BY_ID_QUERY = defineQuery(
+  `
+  *[_type == "startup" && _id == $id][0] {
+  _id, 
+   title, 
+  slug, 
+  _createdAt,
+  author -> {
+    _id, name, image, bio, username
+  }, 
+  views,
+  description, 
+  category, 
+  image,
+  pitch
+}
+  `
 );
